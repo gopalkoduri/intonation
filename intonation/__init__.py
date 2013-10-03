0eb82a0e62d86525dab62585321164173b8f8bab
gopalkoduri <gopala.koduri@gmail.com>
2013-10-03 21:41:42 +0200

    Moving around the files to package the code to pypi.

---
+++ intonation/__init__.py
@@ -0,0 +1,7 @@
+from recording import Recording
+from pitch import Pitch
+from pypeaks import Data, Intervals
+
+del recording
+del pitch
+del pypeaks
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pypeaks',
+      version='0.1',
+      description='Python module to analyze, study and research the intonation
+      of various intervals in different music traditions around the world',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='python intonation research music intervals tuning',
+      url='https://github.com/gopalkoduri/intonation',
+      author='Gopala Krishna Koduri',
+      author_email='gopala.koduri@gmail.com',
+      license='GNU Affero GPL v3',
+      packages=['intonation'],
+      #data_files=[('examples', ['howto.ipynb', 'howto.py', 'ji-intervals.pickle' 'sample-histogram.pickle'])],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+      zip_safe=False)
